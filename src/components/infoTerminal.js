32ca89d0d8999c3c9996b38abb28806b7909c88b
doDigitalUA <81933490+doDigitalUA@users.noreply.github.com>
2021-09-22 19:03:54 +0300

    Update infoTerminal.js

--- src/components/infoTerminal.js
+++ src/components/infoTerminal.js
@@ -11,7 +11,7 @@ function Info() {
 
         const headers = {
            
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'same-origin',
     'Authorization': 'Basic Y2hhdC1CMHQ6NWVIaFY5VDIzYw==', 
     'Content-Type': 'application/json'
   }
